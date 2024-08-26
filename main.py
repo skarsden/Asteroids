c2a0ca5755375aeeaea8efe18de4f22906bbeb6f
skarsden <zachfrancis98@gmail.com>
2024-08-26 11:45:12 -0400

    added quit condition

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
